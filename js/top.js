a62e164eeca688c56883cdd64e317ac7e5178612
한자랑 <hanjarang@hanjalang-ui-MacBookAir.local>
2024-02-26 16:35:48 +0900

    piusjs

---
+++ js/top.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    // $("html,body").css({
+    //     "background-color":"red"
+    // })
+    
+    $(".m-menu span").click(function(){
+        $(".drop").hide()
+        $(this).children(".drop").show().css({
+            display:"flex"
+        });
+    });
+});
